c59ada3d1304308fb04fe8499781b3291b509ea3
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-06-28 12:25:15 +0000

    [update]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+  get '/about', to: 'homes#about'
+  devise_for :users
+
+  resources :books
+
+  resources :users, only: [:show, :edit, :update, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
 end
